5936fd154f2520f2fd39bc2fab1d0ec589e96dd9
claire <claire.liang@elitesland.com>
2021-10-07 04:01:39 +0800

    add env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.NODE_ENV);
   return (
     <div className="App">
       <header className="App-header">
